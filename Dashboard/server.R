60e3ad1546e9c15b4f9dd955edd680538a598fb1
Yiming-Gao <gaoyiming0418@gmail.com>
2018-06-13 16:36:51 -0700

    Update server.R

--- Dashboard/server.R
+++ Dashboard/server.R
@@ -7,19 +7,9 @@
 #    http://shiny.rstudio.com/
 # Note that global.R will be loaded before either ui.R or server.R
 
-setwd("/Users/ygao/Desktop/Dashboard/Code/try")
-options(scipen = 999)
-
-# Fetch the data
-query <- paste0("select distinct run_date, cyence_id, cy, mo, sus from model_monthly_v3.score where run_date >= '", "2018-06-01", "' limit 10")
-scores <- dbGetQuery(con_postgresql, query) %>% as.data.table()
-scores$cyence_id <- as.character(scores$cyence_id)
-# scores <- scores[order(cyence_id, run_date)]
-dbDisconnect(con_postgresql)
-
 # Define server logic required to draw a histogram
 shinyServer(function(input, output) {
-
+  
   output$plot <- renderPlot({
 
     # Render plots based on input$variable from ui.R
