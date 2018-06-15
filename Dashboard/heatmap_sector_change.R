12980de613614029cc4fa63d50c278fc644dbf28
Yiming-Gao <gaoyiming0418@gmail.com>
2018-06-15 15:36:19 -0700

    Update heatmap_sector_change.R

--- Dashboard/heatmap_sector_change.R
+++ Dashboard/heatmap_sector_change.R
@@ -1,13 +1,12 @@
 # Reference: https://learnr.wordpress.com/2010/01/26/ggplot2-quick-heatmap-plotting/
 
-
-
 setwd("/Users/ygao/Desktop/Dashboard/Code/try")
 options(scipen = 999)
 
 #-------------------------------------------------------------------------------------------------------------------------
 ### Supporting functions
 #-------------------------------------------------------------------------------------------------------------------------
+rundate = as.Date('2018-06-01')
 # Connect to databases
 drv <- dbDriver("PostgreSQL")
 
@@ -69,10 +68,19 @@ sector_scores_avg <- companies_scores %>% # order by descending cyence_id
 
 # basic heatmap
 p <- ggplot(sector_scores_avg, aes(revenue_bins, cyence_sector,
+                                   # customize the tooltip
                                    text = paste("Sector: ", cyence_sector, "\n",
                                                 "Number of Companies: ", n_companies, "\n",
                                                 "Change in score: ", sprintf("%.3f %%", 100 * cy_pct_change)))) + 
   geom_tile(aes(fill = cy_pct_change), colour = "white") + 
-  scale_fill_gradient2(low = "red", mid = "white", high = "steelblue", midpoint = 0, limits = range(sector_scores_avg$cy_pct_change))
+  scale_fill_gradient2(low = "steelblue", mid = "white", high = "red", midpoint = 0, limits = range(sector_scores_avg$cy_pct_change), name = "Change (%)") +
+  labs(title = paste0("Change (%) in Cyence Score from \n", month.abb[month(rundate)-1], " to ", month.abb[month(rundate)])) +
+  xlab("Revenue Bins") + ylab("Cyence Sector") +
+  theme(
+    plot.title = element_text(size = 16, face = "bold.italic"),
+    axis.title.x = element_text(size = 12, face = "bold"),
+    axis.title.y = element_text(size = 12, face = "bold"),
+    text = element_text(size = 12)
+  ) 
 
 ggplotly(p, tooltip = c("text"))
