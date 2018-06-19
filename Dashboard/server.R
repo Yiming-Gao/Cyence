f0f73dd9ecc2977be8d1f910eb775b54e919b9e4
Yiming-Gao <gaoyiming0418@gmail.com>
2018-06-19 16:49:39 -0700

    Update server.R

--- Dashboard/server.R
+++ Dashboard/server.R
@@ -13,34 +13,62 @@ shinyServer(function(input, output) {
   output$map <- renderLeaflet({
     # Map by country
     countries <- readOGR(dsn = getwd() , layer = "TM_WORLD_BORDERS_SIMPL-0.3") # https://www.r-graph-gallery.com/183-choropleth-map-with-leaflet/
+    countries@data <- right_join(country_scores_avg, countries@data, by = c("country" = "NAME"))
     # summary(countries$NAME)
     
-    map <- leaflet(countries) %>% addTiles() %>% setView(lat = 40, lng = -20, zoom = 2)
+    map <- leaflet(countries) %>% addTiles() %>% setView(lat = 46, lng = -41, zoom = 3)
     pal <- colorNumeric(
       palette = "YlOrBr",
-      domain = country_scores_avg$country_cy_avg
+      domain = countries$country_cy_avg,
+      na.color = "#f2f0ea"
     )
     
     # Prepare the text for the popup message
     mytext <- 
-      paste("<b>Country: ", countries$NAME,"</b><br/>", 
-            "Number of Companies: ", country_scores_avg$n_companies, "<br/>",
-            "<b>Cyence Score: ", round(country_scores_avg$country_cy_avg, 2), "</b><br/>", 
-            "Susceptibility: ", round(country_scores_avg$country_sus_avg, 2), "<br/>",
-            "Motivation: ", round(country_scores_avg$country_mo_avg,2)) %>%
+      paste("<b>Country: ", countries$country,"</b><br/>", 
+            "Number of Companies: ", countries$n_companies, "<br/>",
+            "<b>Cyence Score: ", round(countries$country_cy_avg, 2), "</b><br/>", 
+            "Susceptibility: ", round(countries$country_sus_avg, 2), "<br/>",
+            "Motivation: ", round(countries$country_mo_avg,2)) %>%
       lapply(htmltools::HTML)
     
     # Display
     map %>%
-      addPolygons(stroke = FALSE, smoothFactor = 0.2, fillOpacity = 0.9, color = "white", weight = 0.3,
-                  fillColor = ~pal(country_scores_avg$country_cy_avg),
+      addPolygons(data = countries, stroke = FALSE, smoothFactor = 0.2, fillOpacity = 0.9, color = "white", weight = 0.3,
+                  fillColor = ~pal(countries$country_cy_avg),
                   label = mytext,
                   labelOptions = labelOptions( style = list("font-weight" = "normal", padding = "3px 8px"), textsize = "13px", direction = "auto")
       ) %>%
-      addLegend("bottomright", pal = pal, values = country_scores_avg$country_cy_avg,
+      addLegend("bottomright", pal = pal, values = countries$country_cy_avg,
                 title = "Average Cyence Score",
                 opacity = 1
       )
   })
+  
+  
+  output$heatmap <- renderPlotly({
+    
+    # basic heatmap
+    p <- ggplot(sector_scores_avg, aes(revenue_bins, cyence_sector,
+                                       # customize the tooltip
+                                       text = paste("Sector: ", cyence_sector, "\n",
+                                                    "Number of Companies: ", n_companies, "\n",
+                                                    "Average score: ", round(sector_cy_avg, 2), "\n",
+                                                    "Change in score: ", round(cy_change, 3)))) + 
+      geom_tile(aes(fill = cy_change), colour = "white") + 
+      scale_fill_gradient2(low = "steelblue", mid = "white", high = "red", midpoint = 0, limits = range(sector_scores_avg$cy_change), name = "Change in Score") +
+      labs(title = paste0("Change in Cyence Score from \n", month.name[month(rundate)-1], " to ", month.name[month(rundate)])) +
+      xlab("Revenue Bins") + ylab("Cyence Sector") +
+      theme(
+        plot.title = element_text(size = 14, face = "bold"),
+        axis.title.x = element_text(size = 12, face = "bold"),
+        axis.title.y = element_text(size = 12, face = "bold"),
+        text = element_text(size = 12)
+      ) 
+    
+    ggplotly(p, tooltip = c("text"))
+    
+  })
+  
   
 })
