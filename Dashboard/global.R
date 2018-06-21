bee915fb18cfbdc35ae04bf19ebb69fe30780e46
Yiming-Gao <gaoyiming0418@gmail.com>
2018-06-21 10:58:27 -0700

    Update global.R
    Display basic functions successfully

--- Dashboard/global.R
+++ Dashboard/global.R
@@ -8,46 +8,76 @@ library(dplyr)
 library(lubridate)
 library(tidyr)
 library(shiny)
+library(shinythemes)
 library(pool)
 library(readr)
 library(plotly)
 library(rgdal)
 library(leaflet)
+library(scales)
 
 
 setwd("/Users/ygao/Desktop/Dashboard/Code/try")
 options(scipen = 999)
-
-# ------------------------------------------------------------ Map ------------------------------------------------------#
-# Fetch the data
 rundate = as.Date('2018-06-01')
+# ------------------------------------------------------------ Map ------------------------------------------------------#
+# # Fetch the data
+# 
+# # Connect to databases
+# drv <- dbDriver("PostgreSQL")
+# 
+# # postgres
+# aws <- Sys.getenv(c("DB_NAME", "DB_HOST", "DB_PORT", "DB_USER", "DB_PASS"))
+# con_postgresql <- dbConnect(drv, 
+#                             dbname = aws["DB_NAME"], 
+#                             host = aws["DB_HOST"], 
+#                             port = aws["DB_PORT"], 
+#                             user = aws["DB_USER"], 
+#                             password = aws["DB_PASS"])
+# 
+# # Cyence score
+# query <- paste0("select distinct run_date, cyence_id, cy, mo, sus from model_monthly_v3.score where run_date >= '", rundate-months(1), "'")
+# scores <- dbGetQuery(con_postgresql, query) %>% as.data.table()
+# scores$cyence_id <- as.character(scores$cyence_id)
+# scores <- scores[order(cyence_id, run_date)]
+# 
+# # US Company Info
+# query <- paste0("select distinct run_date, cyence_id, company_name, website, city, state, country, cyence_sector, sic4, revenue, income, employees from er.company_info where run_date >= '", rundate-months(1), "' 
+#                 and country in (select name from appcache.vw_country where included) and (gap_flag = 1 or revenue >= 20) and gov_flag = 0 and cyence_sector <> 'Public Administration'")
+# us_companies <- dbGetQuery(con_postgresql, query) %>% as.data.table()
+# us_companies$cyence_id <- as.character(us_companies$cyence_id)
+# us_companies <- us_companies[order(cyence_id, run_date)]
+# 
+# 
+# # Data Preparation for top15
+# # The data is re-pulled every time the we provide a new cyence_id
+# query <- paste0("select distinct cyence_id, company_name, cyence_sector, revenue, income, employees from er.company_info where run_date >= '", rundate-months(5), "' 
+#                 and country in (select name from appcache.vw_country where included) and (gap_flag = 1 or revenue >= 20) and gov_flag = 0 and cyence_sector <> 'Public Administration'")
+# ex_company <- dbGetQuery(con_postgresql, query) %>% as.data.table()
+# ex_company$cyence_id <- as.character(ex_company$cyence_id)
+# # ex_company <- ex_company[order(run_date)]
+# 
+# # Cyence score
+# query <- paste0("select distinct run_date, cyence_id, cy, mo, sus from model_monthly_v3.score where run_date >= '", rundate-months(5), "'
+#                 and cyence_id in (select cyence_id from er.company_info)")
+# ex_scores <- dbGetQuery(con_postgresql, query) %>% as.data.table()
+# ex_scores$cyence_id <- as.character(ex_scores$cyence_id)
+# ex_scores <- ex_scores[order(run_date)]
+# dbDisconnect(con_postgresql)
 
-# Connect to databases
-drv <- dbDriver("PostgreSQL")
+# # write to csv
+# write.csv(us_companies, "us_companies.csv")
+# write.csv(scores, "scores.csv")
+# write.csv(ex_company, "ex_company.csv")
+# write.csv(ex_scores, "ex_scores.csv")
 
-# postgres
-aws <- Sys.getenv(c("DB_NAME", "DB_HOST", "DB_PORT", "DB_USER", "DB_PASS"))
-con_postgresql <- dbConnect(drv, 
-                            dbname = aws["DB_NAME"], 
-                            host = aws["DB_HOST"], 
-                            port = aws["DB_PORT"], 
-                            user = aws["DB_USER"], 
-                            password = aws["DB_PASS"])
 
-# Cyence score
-query <- paste0("select distinct run_date, cyence_id, cy, mo, sus from model_monthly_v3.score where run_date >= '", rundate-months(1), "'")
-scores <- dbGetQuery(con_postgresql, query) %>% as.data.table()
-scores$cyence_id <- as.character(scores$cyence_id)
-scores <- scores[order(cyence_id, run_date)]
-
-# US Company Info
-query <- paste0("select distinct run_date, cyence_id, company_name, website, city, state, country, cyence_sector, sic4, revenue, income, employees from er.company_info_union where run_date >= '", rundate-months(1), "' 
-                and country in (select name from appcache.vw_country where included) and (gap_flag = 1 or revenue >= 20) and gov_flag = 0 and cyence_sector <> 'Public Administration'")
-us_companies <- dbGetQuery(con_postgresql, query) %>% as.data.table()
-us_companies$cyence_id <- as.character(us_companies$cyence_id)
-us_companies <- us_companies[order(cyence_id, run_date)]
-
-dbDisconnect(con_postgresql)
+# read csv
+us_companies = read.csv("us_companies.csv")
+scores = read.csv("scores.csv")
+ex_company = read.csv("ex_company.csv")
+ex_scores = read.csv("ex_scores.csv")
+ex_scores$cyence_id = as.character(ex_scores$cyence_id)
 
 # Data Manipulation for Maps
 companies_scores <- inner_join(us_companies, scores, by = "cyence_id") %>% 
@@ -61,13 +91,13 @@ country_scores_avg <- companies_scores %>% # order by descending cyence_id
   summarise(country_cy_avg = mean(cy),
             country_sus_avg = mean(sus),
             country_mo_avg = mean(mo),
-            n_companies = n())
+            n_companies = n()/2)
 
 
 
 # Data Manipulation for heatmaps
 # Left join scores& Company Info by cyence_id
-companies_scores <- inner_join(scores, companies, by = c("cyence_id" = "cyence_id", "run_date" = "run_date")) %>% 
+companies_scores <- inner_join(scores, us_companies, by = c("cyence_id" = "cyence_id", "run_date" = "run_date")) %>% 
   filter(!is.na(company_name)) %>% # filter NAs in company_name column
   # filter(.$country == "United States") %>%
   arrange(desc(cyence_id)) 
