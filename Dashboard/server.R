8dbaa08ff0b8de8d2788da90050de10059dfb44e
Yiming-Gao <gaoyiming0418@gmail.com>
2018-06-21 10:57:26 -0700

    Update server.R
    Display basic functions successfullly

--- Dashboard/server.R
+++ Dashboard/server.R
@@ -10,6 +10,7 @@
 # Define server logic required to draw a histogram
 shinyServer(function(input, output) {
   
+  # Output 1
   output$map <- renderLeaflet({
     # Map by country
     countries <- readOGR(dsn = getwd() , layer = "TM_WORLD_BORDERS_SIMPL-0.3") # https://www.r-graph-gallery.com/183-choropleth-map-with-leaflet/
@@ -45,7 +46,7 @@ shinyServer(function(input, output) {
       )
   })
   
-  
+  # Output 2
   output$heatmap <- renderPlotly({
     
     # basic heatmap
@@ -70,5 +71,67 @@ shinyServer(function(input, output) {
     
   })
   
+  # Output 3
+  output$top10 <- renderPlotly({
+    
+    # Left join scores& Company Info by cyence_id
+    ex_company <- ex_company[ex_company$cyence_sector == input$sector, ]
+    ex_company$cyence_id <- as.character(ex_company$cyence_id)
+    ex_scores <- ex_scores[ex_scores$cyence_id %in% ex_company$cyence_id, ]
+    ex_company_scores <- inner_join(ex_company, ex_scores, by = c("cyence_id" = "cyence_id"))
+    ex_company_scores$run_date <- as.Date(ex_company_scores$run_date)
+    
+    top_unique = ex_company_scores %>%
+      group_by(cyence_id) %>%
+      mutate(cy_change = cy - lag(cy)) %>%
+      filter(run_date == rundate) %>%
+      arrange(desc(cy_change)) %>%
+      distinct(cyence_id)
+    
+    
+    ex_company_scores = ex_company_scores[ex_company_scores$cyence_id %in% as.character(top_unique[1:10,]$cyence_id), ]
+    
+    myplots <- list()
+    
+    # for loop
+    for (i in 1:10) {
+      ex_company_scores_plot <- ex_company_scores[ex_company_scores$cyence_id == as.character(top_unique[1:10,]$cyence_id)[i], ] 
+      ex_company_scores_plot <- melt(ex_company_scores_plot[, c("run_date", "cy", "sus", "mo")], id = "run_date")
+      ex_company_scores_plot$main1 =  paste0("Cyence Scores for ", unique(ex_company_scores[ex_company_scores$cyence_id == as.character(top_unique[1:10,]$cyence_id)[i], ]$company_name))
+      
+      p <- ggplot(ex_company_scores_plot, aes(x = run_date, y = value, colour = variable)) + 
+        geom_line() +
+        (scale_x_date(breaks = date_breaks("1 month"),
+                      labels = date_format("%b %y"),
+                      limits = as.Date(c(rundate-months(5), rundate-months(0))))) +
+        xlab("Run Date") + ylab("Score") + 
+        theme(
+          plot.title = element_text(size = 14, face = "bold"),
+          axis.title.x = element_text(size = 12, face = "bold"),
+          axis.title.y = element_text(size = 12, face = "bold"),
+          text = element_text(size = 12),
+          legend.position="none"
+        ) +
+        facet_wrap(~main1) + 
+        theme_minimal() +
+        geom_point(show.legend = FALSE) +
+        guides(colour = FALSE)
+      
+      myplots[[i]] <- p
+    }
+    
+    subplot(ggplotly(myplots[[1]]) %>% layout(legend = list(x = 0.1, y = 0.9)), 
+            ggplotly(myplots[[2]]) %>% layout(legend = list(x = 0.1, y = 0.9)), 
+            ggplotly(myplots[[3]]) %>% layout(legend = list(x = 0.1, y = 0.9)), 
+            ggplotly(myplots[[4]]) %>% layout(legend = list(x = 0.1, y = 0.9)), 
+            ggplotly(myplots[[5]]) %>% layout(legend = list(x = 0.1, y = 0.9)),
+            ggplotly(myplots[[6]]) %>% layout(legend = list(x = 0.1, y = 0.9)), 
+            ggplotly(myplots[[7]]) %>% layout(legend = list(x = 0.1, y = 0.9)), 
+            ggplotly(myplots[[8]]) %>% layout(legend = list(x = 0.1, y = 0.9)), 
+            ggplotly(myplots[[9]]) %>% layout(legend = list(x = 0.1, y = 0.9)), 
+            ggplotly(myplots[[10]]) %>% layout(legend = list(x = 0.1, y = 0.9)),
+            nrows = 5) %>% layout(title = "Companies with most changes in this month", showlegend = F)
+    
+  })
   
 })
