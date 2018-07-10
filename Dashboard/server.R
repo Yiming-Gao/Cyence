5c89ce1e5a2d1cf044b3d7b550fd875c9010e0d2
Yiming-Gao <gaoyiming0418@gmail.com>
2018-07-10 13:34:07 -0700

    Update server.R

--- Dashboard/server.R
+++ Dashboard/server.R
@@ -223,7 +223,7 @@ Any z-score greater than 3 or less than -3 could be considered as an outlier. Yo
     countries@data <- right_join(country_scores_avg, countries@data, by = c("country" = "NAME"))
     # summary(countries$NAME)
     
-    map <- leaflet(countries) %>% addTiles() %>% setView(lat = 46, lng = -41, zoom = 3)
+    map <- leaflet(countries) %>% addTiles() %>% setView(lat = 46, lng = 12, zoom = 3.5)
     pal <- colorNumeric(
       palette = "YlOrBr",
       domain = countries$country_cy_avg,
@@ -410,6 +410,176 @@ Any z-score greater than 3 or less than -3 could be considered as an outlier. Yo
   })
   
   output$text_diverging_eu <- renderPrint({
+    cat("Cyence ratings have been normalized as z score. Those 20 companies with score above zero are marked red and those 20 below are marked green. 
+        Any z-score greater than 3 or less than -3 could be considered as an outlier. You can check them manually in our app.")
+  })
+  
+  
+  
+  
+  
+  
+  ######################## JP #########################
+  # JP Output 1
+  output$heatmap_jp <- renderPlotly({
+    
+    # read data
+    sector_scores_avg = read.csv("sector_scores_avg_jp.csv")
+    var = input$score_type_jp
+    
+    # basic heatmap
+    p <- ggplot(sector_scores_avg, aes(revenue_bins, cyence_sector,
+                                       # customize the tooltip
+                                       text = paste("Sector: ", cyence_sector, "\n",
+                                                    "Number of Companies: ", n_companies, "\n",
+                                                    "Average score: ", round(sector_scores_avg[, names(sector_scores_avg) == as.name(paste0("sector_", var, "_avg"))], 2), "\n",
+                                                    "Change in score: ", round(sector_scores_avg[, names(sector_scores_avg) == as.name(paste0(var, "_change"))], 2)))) + 
+      geom_tile(aes(fill = sector_scores_avg[, names(sector_scores_avg) == as.name(paste0(var, "_change"))]), colour = "white") + 
+      scale_y_discrete(limits = rev(unique(sector_scores_avg$cyence_sector))) +
+      scale_x_discrete(limits = c("0-5M", "5-10M", "10-25M", "25-50M", "50-100M", "100-500M", "500M-1B", "1-5B", "5-10B", "10B& up")) +
+      scale_fill_gradient2(low = "steelblue", mid = "white", high = "red", midpoint = 0, limits = range(sector_scores_avg[, names(sector_scores_avg) == as.name(paste0(var, "_change"))]), name = "Change in Score") +
+      labs(title = paste0("Score Changes from \n", month.name[month(rundate)-1], " to ", month.name[month(rundate)])) +
+      xlab("Revenue Bins") + ylab("Cyence Sector") +
+      theme(
+        plot.title = element_text(size = 14, face = "bold"),
+        axis.title.x = element_text(size = 12, face = "bold"),
+        axis.title.y = element_text(size = 12, face = "bold"),
+        text = element_text(size = 12)
+      ) 
+    
+    ggplotly(p, tooltip = c("text"))
+    
+  })
+  
+  
+  # JP Output 2: need update every month
+  output$text_revenue_bin_jp <- renderPrint({
+    cat("Ten companies with greatest Cyence rating change in ", input$sector_jp, " with revenue ", input$revenue_bin_jp, " from June to July.")
+  })
+  
+  
+  
+  # JP Output 3
+  output$top10_jp <- renderPlotly({
+    
+    ex_company_scores <- read.csv(paste(input$sector_jp, "_jp.csv", sep = ""))
+    ex_company_scores <- ex_company_scores[ex_company_scores$revenue_bins == input$revenue_bin_jp, ]
+    ex_company_scores_temp = ex_company_scores %>% group_by(cyence_id, run_date) %>% 
+      summarise(cy = mean(cy), mo = mean(mo), sus = mean(sus))
+    ex_company_scores_temp = ex_company_scores_temp %>% group_by(cyence_id) %>% summarise(n_months = n())
+    
+    if (input$gap_or_not_jp == FALSE) {
+      top_unique <- unique(ex_company_scores_temp[ex_company_scores_temp$n_months == 6, ]$cyence_id)
+    }
+    
+    else top_unique <- unique(ex_company_scores$cyence_id)
+    
+    myplots <- list()
+    
+    # for loop
+    for (i in 1:10) {
+      
+      # require certain condition
+      req(length(top_unique) >= 10)
+      
+      ex_company_scores_plot <- ex_company_scores[ex_company_scores$cyence_id == top_unique[i], ]
+      ex_company_scores_plot <- melt(ex_company_scores_plot[, c("run_date", "cy", "sus", "mo")], id = "run_date")
+      ex_company_scores_plot$main1 =  paste0("Cyence Scores for ", unique(ex_company_scores[ex_company_scores$cyence_id == top_unique[i], ]$company_name))
+      ex_company_scores_plot$run_date = as.Date(ex_company_scores_plot$run_date)
+      
+      p <- ggplot(ex_company_scores_plot, aes(x = run_date, y = value, colour = variable)) + 
+        geom_line() +
+        (scale_x_date(breaks = date_breaks("1 month"),
+                      labels = date_format("%b %y"),
+                      limits = as.Date(c(rundate-months(5), rundate-months(0))))) +
+        xlab("Run Date") + ylab("Score") +
+        theme(
+          plot.title = element_text(size = 14, face = "bold"),
+          axis.title.x = element_text(size = 12, face = "bold"),
+          axis.title.y = element_text(size = 12, face = "bold"),
+          text = element_text(size = 12),
+          legend.position="none"
+        ) +
+        facet_wrap(~main1) + 
+        theme_minimal() +
+        geom_point(show.legend = FALSE) +
+        guides(colour = FALSE) 
+      
+      # Force y-axis to be the same
+      if (input$same_y_scale_or_not_jp == FALSE) {
+        p <- p + ylim(range(ex_company_scores_plot$value)[1], range(ex_company_scores_plot$value)[2])
+      }
+      
+      else p <- p + ylim(min(range(ex_company_scores[ex_company_scores$cyence_id %in% top_unique, ]$cy)[1], range(ex_company_scores[ex_company_scores$cyence_id %in% top_unique, ]$mo)[1],range(ex_company_scores[ex_company_scores$cyence_id %in% top_unique, ]$sus)[1]),
+                         max(range(ex_company_scores[ex_company_scores$cyence_id %in% top_unique, ]$cy)[2], range(ex_company_scores[ex_company_scores$cyence_id %in% top_unique, ]$mo)[2],range(ex_company_scores[ex_company_scores$cyence_id %in% top_unique, ]$sus)[2]))
+      
+      myplots[[i]] <- p
+    }
+    
+    subplot(ggplotly(myplots[[1]]), ggplotly(myplots[[2]]), 
+            ggplotly(myplots[[3]]), ggplotly(myplots[[4]]), 
+            ggplotly(myplots[[5]]), ggplotly(myplots[[6]]),
+            ggplotly(myplots[[7]]), ggplotly(myplots[[8]]),
+            ggplotly(myplots[[9]]), ggplotly(myplots[[10]]),
+            nrows = 5) %>% layout(showlegend = F)
+    
+  })
+  
+  
+  # JP Output 4
+  output$diverging_jp <- renderPlotly({
+    
+    ex_company_scores1 <- read.csv(paste(input$sector_jp, "_jp.csv", sep = ""))
+    ex_company_scores1 <- ex_company_scores1[ex_company_scores1$revenue_bins == input$revenue_bin_jp, ]
+    
+    # tell gap or not
+    ex_company_scores1_temp = ex_company_scores1 %>% group_by(cyence_id) %>% summarise(n_months = n())
+    
+    
+    if (input$gap_or_not_jp == FALSE) {
+      ex_company_scores1 <- ex_company_scores1[ex_company_scores1$cyence_id %in% (ex_company_scores1_temp[ex_company_scores1_temp$n_months == 6, ]$cyence_id), ]
+    }
+    
+    else ex_company_scores1 <- ex_company_scores1
+    
+    # data preparation
+    ex_company_scores1 <- ex_company_scores1 %>% group_by(company_name) %>% summarise(cy_avg = mean(cy))
+    ex_company_scores1$cy_avg_z <- round((ex_company_scores1$cy_avg - mean(ex_company_scores1$cy_avg))/ sd(ex_company_scores1$cy_avg), 2)
+    ex_company_scores1$cy_avg_type <- ifelse(ex_company_scores1$cy_avg_z < 0, "below average (safe)", "above average (risky)")
+    ex_company_scores1 <- ex_company_scores1[order(ex_company_scores1$cy_avg_z), ]
+    ex_company_scores1 <- rbind(head(ex_company_scores1, 20), tail(ex_company_scores1, 20))
+    ex_company_scores1$company_name <- factor(ex_company_scores1$company_name, levels = unique(ex_company_scores1$company_name)) # convert to factor to retain sorted order in plot
+    
+    # Require certain condition
+    req(nrow(ex_company_scores1) > 0)
+    
+    # Diverging Barcharts
+    theme_set(theme_bw())
+    
+    p <- ggplot(ex_company_scores1, aes(x = company_name, y = cy_avg_z, label = cy_avg_z,
+                                        # customize the tooltip
+                                        text = paste("Normalized score: ", cy_avg_z))) +
+      geom_bar(stat = "identity", aes(fill = cy_avg_type), width = 0.4) +
+      scale_fill_manual(name = "Risk",
+                        labels = c("Above Average", "Below Average"),
+                        values = c("above average (risky)" = "#f8766d", "below average (safe)" = "#00ba38")) +
+      labs(subtitle = "Normalized cyence scores",
+           title = "Diverging Bars") +
+      ylab("Normalized Score") + xlab("Company Name") +
+      coord_flip()
+    
+    
+    ggplotly(p, tooltip = c("text"))
+    
+  })
+  
+  
+  # JP Some text output
+  output$legend_explanation_jp <- renderPrint({
+    cat("Red cells have their scores increased (riskier), and blue cells have their scores decreased.")
+  })
+  
+  output$text_diverging_jp <- renderPrint({
     cat("Cyence ratings have been normalized as z score. Those 20 companies with score above zero are marked red and those 20 below are marked green. 
         Any z-score greater than 3 or less than -3 could be considered as an outlier. You can check them manually in our app.")
   })
